4f5ddaa7556c839c46057e8bd4f5d495067c389c
李忠贤 <282204626@qq.com>
2021-05-28 16:06:53 +0800

    header和request解除关联

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,6 +8,7 @@ export const LOGIN_PATH:string = '/login'; //登录的路径
 export const USER_AUTHORITY: string = 'admin'; //用户角色,在authority数组去寻找是否有这个角色，有则显示，没有则不渲染
 
 export const USER_TOKEN_NAME:string = 'USER_TOKEN';  //TOKEN字段
+export const USER_INFO_NAME:string = 'USER_INFO_NAME';  //userinfo
 
 export const REQUEST_SUCCESS:number = 200;  //请求成功的标识
 
