efca951d2621285a71af8ff293faff65012cd8db
vlinr <282204626@qq.com>
2021-12-14 14:58:45 +0800

    feat: 更新yarn.lock

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { store } from '@/models/store';
 import setHtmlFontSize from '@/utils/setHtmlFontSize';
 import { VIEW_TO_REM_INFO } from './config/config';
 import LocaleConfigProvider from './LocaleConfigProvider';
-setHtmlFontSize(VIEW_TO_REM_INFO); // 初始化得时候设置一下字体大小
-
+setHtmlFontSize(VIEW_TO_REM_INFO); //初始化得时候设置一下字体大小
+const a={1:1,2:2}
 ReactDOM.render(
     <Provider store={store}>
         <LocaleConfigProvider>
