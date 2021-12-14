08c234d2029ddebf66566ad638b9d422ea8da800
vlinr <282204626@qq.com>
2021-12-14 14:57:27 +0800

    feat: 更新yarn.lock

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'package.json': ['npx prettier -w  package.json'],
     '*.tsx,*.js,*.ts,*.jsx': ['npm run lint:fix', 'npx prettier -w "src/**/*.(tsx|js|ts|jsx)"'],
-    '*.css,*.less,*.scss': ['npm run stylelint', 'npx prettier -w "src/**/*.(less|scss|css)"'],
+    '*.css,*.less,*.scss': ['npm run lint:style', 'npx prettier -w "src/**/*.(less|scss|css)"'],
     // '*.md': ['npm run markdownlint', 'npm run prettier'],
 };
