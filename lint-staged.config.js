12b8fcf9b281740115f3bb799ce77239050e4195
vlinr <282204626@qq.com>
2021-12-15 17:30:18 +0800

    feat: 修改无法打包问题

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     'package.json': ['npx prettier -w  package.json', 'git add'],
     '*.{tsx,js,ts,jsx}': [
-        'npm run lint:fix',
         'npx prettier -w "src/**/*.(tsx|js|ts|jsx)"',
+        'npm run lint:fix',
         'git add',
     ],
     '*.{less,scss,css}': [
-        'npm run lint:style',
         'npx prettier -w "src/**/*.(less|scss|css)"',
+        'npm run lint:style',
         'git add',
     ],
     // '*.md': ['npm run markdownlint', 'npm run prettier'],
