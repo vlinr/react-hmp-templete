0eb90fd7d79de601747c2a5e7321fe6e19f6305e
vlinr <282204626@qq.com>
2021-12-16 17:44:43 +0800

    feat: 修改husky安装问题

--- .init.js
+++ .init.js
@@ -3,15 +3,19 @@ const path = require('path');
 (() => {
     let result = fs.existsSync(path.join(__dirname, '.husky'));
     if (!result) {
-        const exec = require('child_process').exec;
-        const husky =
-            "npx husky add .husky/commit-msg 'npx commitlint --edit $1' && npx husky add .husky/pre-commit 'npx lint-staged'";
-        const cmd = `husky install && ${husky}`;
-        exec(cmd, (err, stdout) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log(`${stdout}`);
+        fs.mkdir('.husky', (err) => {
+            if (!err) {
+                const exec = require('child_process').exec;
+                const husky =
+                    "npx husky add .husky/commit-msg 'npx commitlint --edit $1' && npx husky add .husky/pre-commit 'npx lint-staged'";
+                const cmd = `npx husky install && ${husky}`;
+                exec(cmd, (err, stdout) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log(`${stdout}`);
+                    }
+                });
             }
         });
     }
