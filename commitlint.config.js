e955eef1c862bcbdb8ccc56d3651756f269e333e
vlinr <282204626@qq.com>
2021-12-27 15:09:53 +0800

    Revert "123"
    This reverts commit 442e982ebb873b53f88c381da9e4e29ed9a9d43b.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    // extends: ['@commitlint/config-conventional'],
-    formatter: '@commitlint/format',
+    extends: ['@commitlint/config-conventional'],
     rules: {
         'type-enum': [
             2,
             'always',
-            ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert', 'build', 'perf'],
+            ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert','build','perf'],
+            
         ],
         'type-case': [0],
         'type-empty': [0],
