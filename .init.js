461cd24ab08559c679588ca64af6919321cc4eae
vlinr <282204626@qq.com>
2021-12-27 15:17:16 +0800

    feat: 增加changelog

--- .init.js
+++ .init.js
@@ -7,7 +7,7 @@ const path = require('path');
             if (!err) {
                 const exec = require('child_process').exec;
                 const husky =
-                    "npx husky add .husky/commit-msg 'npx commitlint --edit $1' && npx husky add .husky/pre-commit 'npx lint-staged' && npx husky add .husky/pre-push 'npx standard-version --preset gitmoji-config'";
+                    "npx husky add .husky/commit-msg 'npx --no -- commitlint --edit $1' && npx husky add .husky/pre-commit 'npx lint-staged' && npx husky add .husky/pre-push 'npx standard-version --preset gitmoji-config'";
                 const cmd = `npx husky install && ${husky}`;
                 exec(cmd, (err, stdout) => {
                     if (err) {
